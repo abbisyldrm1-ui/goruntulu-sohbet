ac7e92540cb7f683a9181fc498f87354a2004c86
REMIXMEN <abbisyldrm1@gmail.com>
2026-05-18 23:08:17 +0300

    register system

--- app/firebase.js
+++ app/firebase.js
@@ -1,14 +1,17 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBco5nTI1dNHHEJHAjEEU_uImP9m_v_qUI",
+  apiKey: "AIzaSyBco5nTI1dNHHEJHAjEEU_uImP9m_v_quI",
   authDomain: "aura-live-a4de5.firebaseapp.com",
   projectId: "aura-live-a4de5",
   storageBucket: "aura-live-a4de5.firebasestorage.app",
   messagingSenderId: "217876355979",
-  appId: "1:217876355979:web:9fb5302a6e9f7cc843ad8e",
+  appId: "1:217876355979:web:da1f18bc394f4fc043ad8e",
 };
 
 const app = initializeApp(firebaseConfig);
 
-export default app;
+export const auth = getAuth(app);
+export const db = getFirestore(app);
